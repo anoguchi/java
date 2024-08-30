a0935d8f346e562d3148f3d52901935296e1302e
Alberto Noguchi <alberto2611@gmail.com>
2024-08-30 20:28:39 -0300

    add: cap02 project

---
+++ 02-Variables_and_Primitive_Data_Types/project/Main.java
@@ -0,0 +1,17 @@
+package project;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Jaum";
+        int age = 55;
+        String species = "Triceratops";
+        String diet = "herbivore";
+        double weight = 8_000; // kg
+
+        String out = "Meet " + name + ", a " + age + " years old " + species + ". As a " + diet +
+                ", it has a robust weight of " + weight + " kilograms.";
+
+        System.out.println(out);
+
+    }
+}
