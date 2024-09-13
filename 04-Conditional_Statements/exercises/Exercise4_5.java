91137f6fba126c14ef950ce4a6709f90a8fbd494
Alberto Noguchi <alberto2611@gmail.com>
2024-09-13 18:31:48 -0300

    add: `cap04` exercise 5

--- 04-Conditional_Statements/exercises/Exercise4_5.java
+++ 04-Conditional_Statements/exercises/Exercise4_5.java
@@ -1,2 +1,28 @@
 public class Exercise4_5 {
+    public static void main(String[] args) {
+        /*
+          Proper housing is essential for the dinosaurs’ well-being. Write a switch statement that assigns a dinosaur to
+          a specific enclosure based on its size (XS, S, M, L, or XL).
+        */
+        String size = "M"; // change this variable to test the code
+
+        switch (size) {
+            case "XS":
+                System.out.println("Assign the dinosaur to the Small Herbivore Enclosure.");
+                break;
+            case "S":
+                System.out.println("Assign the dinosaur to the Medium Herbivore Enclosure.");
+                break;
+            case "M":
+                System.out.println("Assign the dinosaur to the Large Herbivore Enclosure.");
+                break;
+            case "L":
+            case "XL":
+                System.out.println("Assign the dinosaur to the Carnivore Enclosure.");
+                break;
+            default:
+                System.out.println("Unknown dinosaur size.");
+                break;
+        }
+    }
 }
