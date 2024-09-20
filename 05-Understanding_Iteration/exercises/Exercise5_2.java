35e4cc08df57ce024fbe5b21a663f02bb72f604e
Alberto Noguchi <alberto2611@gmail.com>
2024-09-20 17:18:50 -0300

    add: `cap05` exercise5_2

---
+++ 05-Understanding_Iteration/exercises/Exercise5_2.java
@@ -0,0 +1,16 @@
+public class Exercise5_2 {
+    public static void main(String[] args) {
+        /*
+         * Some of our dinosaurs have large appetites! Write a do-while loop that continues to feed a dinosaur until it
+         * is no longer hungry.
+         */
+
+        boolean hungry = true;
+        do {
+            System.out.println("Feeding dinosaur...");
+            System.out.println("Feeding dinosaur...");
+            System.out.println("Feeding dinosaur...");
+            hungry = false;
+        } while (hungry);
+    }
+}
