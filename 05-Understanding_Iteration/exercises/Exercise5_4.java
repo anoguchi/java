123d7e56450999410e03e54eab033d778be5f142
Alberto Noguchi <alberto2611@gmail.com>
2024-09-20 17:29:16 -0300

    add: `cap05` exercise5_4

---
+++ 05-Understanding_Iteration/exercises/Exercise5_4.java
@@ -0,0 +1,16 @@
+public class Exercise5_4 {
+    public static void main(String[] args) {
+        /*
+         * For planning purposes, it's essential to know the total weight of all dinosaurs in a specific enclosure.
+         * Write a for loop that calculates this.
+         */
+
+        int[] weights = {1000, 2000, 3000, 4000, 5000};
+        int totalWeight = 0;
+
+        for (int weight : weights) {
+            totalWeight += weight;
+        }
+        System.out.println("Total weight of dinosaurs: " + totalWeight);
+    }
+}
